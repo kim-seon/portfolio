7de0844a0cd861ab4917cf0370710fa8a0ac9c74
kim-seon <91369649+kim-seon@users.noreply.github.com>
2021-12-08 18:31:04 +0900

    JS: navbar menu click

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark')
     }
 });
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+});
+
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+
+})
